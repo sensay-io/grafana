bee63a736dcd231e79fe09318aca79b876f19c05
Michele Di Cosmo <micheledicosmo@gmail.com>
2025-08-13 15:25:31 +0200

    Switch to sed for templating

--- prometheus/docker-entrypoint.sh
+++ prometheus/docker-entrypoint.sh
@@ -5,8 +5,10 @@ set -eu
 : "${SUPABASE_PROJECT_REF:?Set SUPABASE_PROJECT_REF env var}"
 : "${SUPABASE_SERVICE_ROLE_KEY:?Set SUPABASE_SERVICE_ROLE_KEY env var}"
 
-# Render template -> final config
-envsubst < /etc/prometheus/prom.yml.tpl > /etc/prometheus/prom.yml
+# Replace environment variable placeholders
+sed -e "s/\${SUPABASE_PROJECT_REF}/${SUPABASE_PROJECT_REF}/g" \
+    -e "s/\${SUPABASE_SERVICE_ROLE_KEY}/${SUPABASE_SERVICE_ROLE_KEY}/g" \
+    /etc/prometheus/prom.yml.tpl > /etc/prometheus/prom.yml
 
 # Exec the original prometheus entrypoint
 exec /bin/prometheus "$@"

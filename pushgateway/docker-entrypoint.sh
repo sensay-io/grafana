bf431cdc77b3d153cb451986fdbdb30f8a296c58
vpashkov <vadim@sensay.io>
2025-09-03 13:58:26 +0400

    write pushgateway config to a temp dir

--- pushgateway/docker-entrypoint.sh
+++ pushgateway/docker-entrypoint.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 
-# Require bcrypt hash in env
-PASSWORD_HASH="${PUSHGATEWAY_HASH:?Set PUSHGATEWAY_HASH to a bcrypt hash}"
+HASH="${PUSHGATEWAY_HASH:?Set PUSHGATEWAY_HASH to a bcrypt hash}"
 
-# Write minimal web.yml
-cat >/etc/pushgateway-web.yml <<EOF
+WEBCFG="/tmp/pushgateway-web.yml" # writable without root
+cat >"$WEBCFG" <<EOF
 basic_auth_users:
-  pusher: "${PASSWORD_HASH}"
+  pusher: "$HASH"
 EOF
 
-# Start Pushgateway on IPv6, port 9091, with auth
 exec /bin/pushgateway \
   --web.listen-address=[::]:9091 \
-  --web.config.file=/etc/pushgateway-web.yml
+  --web.config.file="$WEBCFG"
